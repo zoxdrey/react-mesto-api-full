8c830da87f162eb89fb4ab4ba5f5a7dfcb673389
zoxdrey <zoxypva@gmail.com>
2021-10-08 21:19:03 +0300

    add winston logger

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
-const { PORT = 3000 } = process.env;
+const {PORT = 3000} = process.env;
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const { celebrate, Joi, errors } = require('celebrate');
+const {celebrate, Joi, errors} = require('celebrate');
 const router = require('./routes');
-const { login, createUser } = require('./controllers/users');
+const {login, createUser} = require('./controllers/users');
 const auth = require('./middlewares/auth');
-const { MONGO_CONNECTION_STRING } = require('./utils/constants');
-const { LINK_REGEXP_VALIDATION_STRING } = require('./utils/constants');
-
+const {MONGO_CONNECTION_STRING} = require('./utils/constants');
+const {LINK_REGEXP_VALIDATION_STRING} = require('./utils/constants');
+const {requestLogger, errorLogger} = require('./middlewares/logger');
 const app = express();
 
 mongoose.connect(MONGO_CONNECTION_STRING, {
@@ -17,38 +17,39 @@ mongoose.connect(MONGO_CONNECTION_STRING, {
 });
 app.use(helmet());
 app.use(bodyParser.json());
-
+app.use(requestLogger);
 app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().min(2).max(30)
-      .email()
-      .required(),
-    password: Joi.string().required().min(2).max(30)
-      .required(),
+    body: Joi.object().keys({
+      email: Joi.string().required().min(2).max(30)
+        .email()
+        .required(),
+      password: Joi.string().required().min(2).max(30)
+        .required(),
+    }),
   }),
-}),
-login);
+  login);
 
 app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(LINK_REGEXP_VALIDATION_STRING),
-    email: Joi.string().required().min(2).max(30)
-      .email()
-      .required(),
-    password: Joi.string().required().min(2).max(30)
-      .required(),
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(LINK_REGEXP_VALIDATION_STRING),
+      email: Joi.string().required().min(2).max(30)
+        .email()
+        .required(),
+      password: Joi.string().required().min(2).max(30)
+        .required(),
+    }),
   }),
-}),
-createUser);
+  createUser);
 app.use(auth);
 app.use('/', router);
 
+app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
-  const { statusCode = 500, message } = err;
+  const {statusCode = 500, message} = err;
 
   res.status(statusCode)
     .send({
